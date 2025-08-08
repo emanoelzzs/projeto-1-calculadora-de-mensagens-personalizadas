57c1893609dbe37e18e20b5854cb18f93e0300d5
Emanoel Alves <joao.silva3984@aluno.ce.gov.br>
2025-08-08 14:52:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let nome = "Dexter Morgan"
 let idade = 54 
 let anoAtual = 2025
-let moradia = "Flórida"
+let moradia = "Miami"
 let profissao = "analista forense"
 let salario = 5000
 let bonus = 1500 
 let totalMes = salario + bonus 
 let totalAno = totalMes * 12 
 let dobroAno = totalAno * 2 
+const anoNascimento = anoAtual - idade
 
-
-alert (`    
+alert (`
+========= RELATÓRIO DO USUÁRIO =========   
  Nome: ${nome}
  Idade: ${idade}
- Ano de nascimento: ${anoAtual - idade}
+ Ano de nascimento: ${anoNascimento}
  Cidade: ${moradia}
  Profissão: ${profissao}
  Salário base: U$${salario}     
@@ -21,6 +22,7 @@ alert (`
  Total mensal: U$${totalMes}
  Total anual: U$${totalAno}
  Total anual (salário dobrado): U$${dobroAno}
+========================================= 
     `)
 
 
